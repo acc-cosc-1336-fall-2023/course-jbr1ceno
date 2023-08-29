2c555da08b7bef9b173b0be24b5a65da85a91ab9
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-08-29 00:30:22 +0000

    Intro dev proc, variables, functions, test case

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,5 +1,15 @@
 import devprocess
 
-result = devprocess.add_numbers(5, 5)
+result = 'Hello World' #will save the Hello World Sequence  to the result variable
 
 print(result)
+
+result = 123
+
+print(result)
+
+result = devprocess.add_numbers(50, 50)
+
+print(result)
+
+
