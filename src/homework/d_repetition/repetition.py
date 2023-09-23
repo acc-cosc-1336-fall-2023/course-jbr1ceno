432c2a4c0e9c93c08ee47372f7a52f964876da3a
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-09-23 22:04:44 +0000

    COSC 1336 HW3 (still working)

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,12 @@
-#
+#for loop
+def get_factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num * get_factorial(num - 1)
+    
+#while loop
+'''def sum_odd_numbers(num):
+    while(num > 0 and num * 2):
+
+        return num'''
