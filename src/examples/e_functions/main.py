d50f60502d7544e6c3a5aad072c1e7263bab1016
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-09-26 00:20:15 +0000

    void functions

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,1 +1,8 @@
 #main program
+import void_functions
+
+
+#void_functions.display()
+
+#DON'T add print to function here: print(void_function.say_hello("Python"))
+void_functions.say_hello("Python")
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -1,1 +1,6 @@
 #
+def display():
+    print("hello world!")
+
+def say_hello(name):
+    print("hello " + name)
