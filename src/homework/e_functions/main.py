c71bda5d277fc2c88387badcf0416e86902ae981
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-01 01:52:43 +0000

    COSC 1336 HW 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,10 @@
-#
+import unittest
+import value_return
+
+from datetime import datetime
+
+hour = str(value_return.get_hour(3800)).zfill(2)
+minute = str(value_return.get_minutes(3800)).zfill(2)
+second = str(value_return.get_seconds(3800)).zfill(2)
+
+print(hour + ":" + minute + ":" + second)
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,24 @@
-#
+from datetime import datetime
+
+#get current day
+dt = datetime.today()
+epoch_seconds = dt.timestamp()
+#print(seconds)
+
+def get_hour(epoch_seconds):
+    hours = epoch_seconds // 3600
+    remHours = hours % 3600
+    #print("Remainder hr: ", remHours)
+    return remHours
+
+def get_minutes(epoch_seconds):
+    minutes = epoch_seconds // 60
+    remMinutes = minutes % 60
+    #print("Remainder min: ", remMinutes)
+    return remMinutes
+
+def get_seconds(epoch_seconds):
+    seconds = epoch_seconds % 3600
+    remSeconds = seconds % 60
+    #print("Remainder sec: ", remSeconds)
+    return remSeconds
