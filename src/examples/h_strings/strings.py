291f0513b1eef1c685515b23f9f0ebe4d2d9ebfd
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-03 00:49:20 +0000

    loop, concat, slice, and slice w step strings

--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -17,3 +17,24 @@ def loop_string_w_for(str):
 def loop_string_w_special_for(str):
     for ch in str:
         print(ch)
+
+def concat_strings(str1, str2):
+    return str1 + str2
+
+def slice_string(str):
+    return str[6:10]
+
+def slice_w_step_value(str):
+    return str[0:len(str):2]
+
+
+#quiz practice
+'''num = 10
+
+def display_num():
+    global num
+    num = 100
+    print(num)
+    
+num = 0
+print(num)'''
