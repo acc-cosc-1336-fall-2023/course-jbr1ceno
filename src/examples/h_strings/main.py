5e0fb98d8f787c785df7f839df4bcf137ed39277
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-03 00:08:31 +0000

    loop strings w while, for, n replace

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,9 +1,20 @@
 #main program
-letter = 'A'
+'''letter = 'A'
 print(letter)
 
 lang = "Python"
 
 print(lang)
 print(len(lang))
+print(lang[2])'''
+
+import strings
+
+#strings.loop_string_w_while("Python")
+#strings.loop_string_w_for("Python")
+#strings.loop_string_w_special_for("Python")
+lang = "Python"
+
 print(lang[2])
+lang = lang.replace('y', 'Y').replace('o', 'O')
+print(lang)
