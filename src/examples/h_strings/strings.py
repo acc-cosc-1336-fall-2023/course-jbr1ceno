3651852fc06ddb110f1f1e002db5bd042c4ae861
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-04 23:34:50 +0000

    string comparison

--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -27,6 +27,11 @@ def slice_string(str):
 def slice_w_step_value(str):
     return str[0:len(str):2]
 
+def search_in_string(str1, str2):
+    return str1 in str2
+
+
+
 
 #quiz practice
 '''num = 10
--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.h_strings.strings import concat_strings, slice_string, slice_w_step_value, test_config
+from src.examples.h_strings.strings import concat_strings, search_in_string, slice_string, slice_w_step_value, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -16,7 +16,24 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(slice_string(name), "Lynn")
         self.assertEqual(name[11:], "Smith")
 
-    '''def test_slice_w_step(self):
+    def test_slice_w_step(self):
         letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.assertCountEqual(slice_w_step_value(letters))'''
+        self.assertEqual(slice_w_step_value(letters), "ACEGIKMOQSUWY")
 
+    def test_compare_strings(self):
+        #Python compare ASCII numerical values in string comparisons
+        str1 = "c" #99
+        str2 = "p" #112
+
+        self.assertEqual(str1 < str2, True)
+        self.assertEqual(str2 < str1, False)
+        self.assertEqual("C++" == "C++ ", False)
+
+    def test_search_in_string(self):
+        str1 = "abc"
+        str2 = "abcdef"
+
+        self.assertEqual(search_in_string(str1, str2), True)
+        self.assertEqual(search_in_string("efg", str2), False)
+
+        
