31fd37430062786333bb61bb9d53746d90c831c0
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-05 00:00:06 +0000

    built-in string functions

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -35,3 +35,43 @@ class Test_Config(unittest.TestCase):
 
         self.assertEqual(search_in_string(str1, str2), True)
         self.assertEqual(search_in_string("efg", str2), False)
+
+    def test_is_string_digit(self):
+        str1 = "1200"
+        self.assertEqual(str1.isdigit(), True)
+        self.assertEqual("ab".isdigit(), False)
+
+    def test_string_is_alpha(self):
+        str1 = "abcdefg"
+        self.assertEqual(str1.isalpha(), True)
+        self.assertEqual("$%1".isalpha(), False)
+
+    def test_string_is_upper(self):
+        str1 = "XYZ"
+        self.assertEqual(str1.isupper(), True)
+
+    def test_string_to_lower(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.lower(), "abcdef")
+
+    def test_string_to_upper(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.upper(), "ABCDEF")
+
+    def test_strip_char_from_strings(self): #gets rid of char from left and right
+        str1 = "eeabcdefegfee"
+        self.assertEqual(str1.strip("e"), "abcdefegf")
+
+    def test_lstrip_char_from_strings(self):
+        str1 = "aaaaxyz"
+        self.assertEqual(str1.lstrip("a"), "xyz")
+
+    def self_rstrip_char_from_strings(self):
+        str1 = "abcyyy"
+        self.assertEqual(str1.rstrip("y"), "abc")
+
+    def test_replace_from_string(self):
+        str1 = "eeabcdefegfee"
+        self.assertEqual(str1.replace("e", ""), "abcdfgf")
+
+    
