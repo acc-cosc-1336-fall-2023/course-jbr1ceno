92456d6352fc3f2859e8e21b1bbc9473b13d6bd7
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-10 00:00:19 +0000

    looping a list

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,10 @@
 #main program
+
+import lists
+
+#lists.display_nums_list()
+#lists.display_list_w_while()
+#lists.display_reverse_list_w_while()
+#lists.display_list_w_for_range()
+#lists.display_list_w_for_range_w_step()
+lists.display_reverse_list_w_for_range()
