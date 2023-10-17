23915e6296be12055b41e9ff7689eb4a541a08f0
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-17 00:17:35 +0000

    list within list preview

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -9,4 +9,19 @@ import lists
 #lists.display_list_w_for_range_w_step()
 #lists.display_reverse_list_w_for_range()
 
-list1 = [5, 10, 20]
+
+list1 = [[5, 10, 20], [6, 10, 5], [5, 0, 20]]
+#print(list1[0][0])
+#print(list1[2])
+
+
+#list_at_indx_0 = list1[0]
+#print(list_at_indx_0)
+#list_at_indx_2 = list1[2]
+list_at_indx_2 = [5, 0, 20]
+print(list_at_indx_2)
+print(list_at_indx_2[1])
+print(list1[2][1])
+print(list1[1][1])
+
+#list2 = ["joe", 10, 10.5, [0, 4, 5]]
