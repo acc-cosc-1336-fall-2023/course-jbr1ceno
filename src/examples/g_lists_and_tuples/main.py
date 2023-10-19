1d7012ad178f60271e16e7753c273c63002e4a08
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-19 00:18:27 +0000

    tuples

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,30 +1,6 @@
 #main program
 
-import lists
+import tuples
 
-#lists.display_nums_list()
-#lists.display_list_w_while()
-#lists.display_reverse_list_w_while()
-#lists.display_list_w_for_range()
-#lists.display_list_w_for_range_w_step()
-#lists.display_reverse_list_w_for_range()
-
-
-#list1 = [[5, 10, 20], [6, 10, 5], [5, 0, 20]]
-#print(list1[0][0])
-#print(list1[2])
-
-
-#list_at_indx_0 = list1[0]
-#print(list_at_indx_0)
-#list_at_indx_2 = list1[2]
-'''list_at_indx_2 = [5, 0, 20]
-print(list_at_indx_2)
-print(list_at_indx_2[1])
-print(list1[2][1])
-print(list1[1][1])'''
-
-#list2 = ["joe", 10, 10.5, [0, 4, 5]]
-
-list1 = lists.get_multiplication_table(3)
-print(list1)
+#tuples.create_tuple()
+tuples.create_list_from_tuple()
