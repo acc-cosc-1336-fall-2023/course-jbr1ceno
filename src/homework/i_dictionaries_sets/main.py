2138d3b539db4c4211628516d996c6301254ca0a
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-22 00:40:47 +0000

    HW 6 WIP

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,30 @@
 #
+import dictionary
+
+def menu():
+    print("1- Get p distance matrix ")
+    print("2- Exit ")
+
+menu()
+option = int(input("Enter your option: "))
+
+while option != 2:
+    if option == 1:
+        listTable = []
+        listInput = ""
+        while(listInput != "STOP"):
+            listInput = str.upper(input("Enter the row or type STOP to finish: "))
+            lengthInput = len(listInput)
+            if(listInput != "STOP"):
+                listTable.append(listInput)
+        print("The matrix is: ", listTable)
+        print("The p distance is: ", str(dictionary.get_p_distance_matrix(listTable)))
+
+    else:
+        print("Invalid option.")
+    
+    print()
+    menu()
+    option = int(input("Enter your option: "))
+
+print("You have exited the program.")
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,35 @@
 #
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance, get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+
+    def test_p_distance(self):
+        list1 = ['T','T','T','C','C','A','T','T','T','A']
+        list2 = ['G','A','T','T','C','A','T','T','T','C']
+        self.assertEqual(4, get_p_distance(list1, list2))
+        #print(get_p_distance(list1, list2))
+
+    def test_get_p_distance_matrix(self):
+        matrix_input = [
+ ['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']
+]
+        matrix_output = [
+ [0.0, 0.4, 0.1, 0.1],
+ [0.4, 0.0, 0.4, 0.3],
+ [0.1, 0.4, 0.0, 0.2],
+[0.1, 0.3, 0.2, 0.0]
+]
+        #Testing for 5
+        #matrix_input1 = [ ['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C'], ['T','T','T','C','C','A','T','T','T','T'], ['G','T','T','C','C','A','T','T','T','A'], ['G','T','A','C','C','A','T','T','T','A']]
+        #matrix_output1 =[[0.0, 0.4, 0.1, 0.1, 0.2], [0.4, 0.0, 0.4, 0.3, 0.4], [0.1, 0.4, 0.0, 0.2, 0.3], [0.1, 0.3, 0.2, 0.0, 0.1], [0.2, 0.4, 0.3, 0.1, 0.0]]
+        #self.assertEqual(matrix_output1, get_p_distance_matrix(matrix_input1))
+        #print(get_p_distance_matrix(matrix_input1))
+
+        self.assertEqual(matrix_output, get_p_distance_matrix(matrix_input))
+        print(get_p_distance_matrix(matrix_input))
+       
