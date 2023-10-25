d645c30285871125e1d49fbaf8f445f5745079c1
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-10-25 23:19:31 +0000

    add, update, delete friend from phonebook

--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.i_dictionaries_sets.dictionaries import is_key_in_dictionary, test_config
+from src.examples.i_dictionaries_sets.dictionaries import add_friend_phonebook, delete_friend_phonebook, is_key_in_dictionary, test_config, update_friend_phonebook
 
 class Test_Config(unittest.TestCase):
 
@@ -12,4 +12,28 @@ class Test_Config(unittest.TestCase):
 
         self.assertEqual(is_key_in_dictionary('Cris', phonebook), False)
         self.assertEqual(is_key_in_dictionary('Chris', phonebook), True)
+
+    def test_add_friend_phonebook(self):
+        phonebook = {}
+        add_friend_phonebook('Chris', '555-1111', phonebook)
+
+        self.assertEqual(phonebook, {'Chris':'555-1111'})
+
+    def test_update_friend_phonebook(self):
+        phonebook = {'Chris':'555-1111'}
+
+        update_friend_phonebook('Chris', '555-1234', phonebook)
+        self.assertEqual(phonebook, {'Chris':'555-1234'})
+
+        update_friend_phonebook('Cris', '555-1234', phonebook)
+        self.assertEqual(phonebook, {'Chris':'555-1234'})
+
+    def test_delete_friend_phonebook(self):
+        phonebook = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+        delete_friend_phonebook('Chris', phonebook)
+        
+        self.assertEqual(phonebook, {'Katie':'555-2222', 'Joanne':'555-3333'})
+
+        delete_friend_phonebook('Kati', phonebook)
+        self.assertEqual(phonebook, {'Katie':'555-2222', 'Joanne':'555-3333'})
 
