0d9c0da568ef88f0651327c88afcf2aa049a01b4
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-11-09 00:39:59 +0000

    bank account deposit and withdraw

--- src/examples/j_classes/bank_account.py
+++ src/examples/j_classes/bank_account.py
@@ -6,3 +6,13 @@ class BankAccount:
 
     def get_balance(self): #other classes can see get_balance
         return self.__balance
+
+    def deposit(self, amount):
+
+        if(amount > 0):
+            self.__balance += amount
+
+    def withdraw(self, amount):
+
+        if(amount > 0 and amount <= self.__balance):
+            self.__balance -= amount
--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -3,3 +3,15 @@ import bank_account
 account = bank_account.BankAccount(50)
 
 print(account.get_balance())
+
+amount = int(input("Enter deposit amount: "))
+
+account.deposit(amount)
+
+print(account.get_balance())
+
+amount = int(input("Enter amount: "))
+
+account.withdraw(amount)
+
+print(account.get_balance())
