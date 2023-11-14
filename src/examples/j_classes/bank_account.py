4130c579e2cc9170bebbbb24c195e9f203b3a34e
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-11-14 00:37:27 +0000

    account private method/function

--- src/examples/j_classes/bank_account.py
+++ src/examples/j_classes/bank_account.py
@@ -1,8 +1,14 @@
+import random
+
 class BankAccount:
     __balance = 0 #__ hide it(variable) from other classes (private)
 
-    def __init__(self, balance): #constructor -- initialize class data/variables
-        self.__balance = balance
+    def __init__(self, balance): #constructor -- initialize(run) class data/variables
+
+        if(balance >= 0):
+            self.__balance = balance
+        else:
+            self.__get_balance_from_db() #private function--not accessible from outside the class
 
     def get_balance(self): #other classes can see get_balance
         return self.__balance
@@ -16,3 +22,6 @@ class BankAccount:
 
         if(amount > 0 and amount <= self.__balance):
             self.__balance -= amount
+
+    def __get_balance_from_db(self): #db means database
+        self.__balance = random.randint(0, 10000)
--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,6 +1,7 @@
 import bank_account, atm, menu
 
 account = bank_account.BankAccount(50)
+
 my_atm = atm.ATM(account)
 
 menu.run_menu(my_atm)
